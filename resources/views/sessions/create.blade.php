e3fcd4ebd3d4fbf40e4ebd02b4fd7fda0170fad1
Shikongo-Veijo <shikongoveijo@gmail.com>
2019-03-18 05:56:50 +0200

    new login and registration

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master')
+{{--@extends('layouts.master')
 
 @section('title')
     Login
@@ -40,8 +40,87 @@
         </div>
 
         <button type="submit" class="btn btn-primary">Login</button>
-        <hr><a href="#">Forget Password?</a>
+        <hr><a href="#">Forget Pas</a>
 
     </form>
 
-@endsection
+@endsection--}}
+
+
+
+
+
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+   <!--Made with love by Mutiullah Samim -->
+   
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/css.css')}}">
+</head>
+<body>
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Sign In</h3>
+				{{--<div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-google-plus-square"></i></span>
+					<span><i class="fab fa-twitter-square"></i></span>
+				</div>--}}
+			</div>
+			<div class="card-body">
+            <form action="/login" method="post">
+
+{{ csrf_field() }}
+                
+        <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="email" name="email" class="form-control" id="email" value="javed@mail.com" placeholder="E-mail">
+						<span class="text-danger">{{ $errors->has('email') ? $errors->first('email') : '' }}</span>
+					</div>
+					
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+                        <input type="password" name="password" class="form-control" id="password" value="secret" placeholder="Password">
+                        <span class="text-danger">{{ $errors->has('password') ? $errors->first('password') : '' }}</span>
+					</div>
+					<div class="row align-items-center remember">
+						<input type="checkbox">Remember Me
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Login" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Don't have an account?<a href="{{url('/registration')}}">Sign Up</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a href="#">Forgot your password?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
